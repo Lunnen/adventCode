8698aa7c020a68630b526f6fdad36104a35d6643
Andreas <69907843+Lunnen@users.noreply.github.com>
2021-12-02 12:18:20 +0100

    Add files via upload

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,25 @@
+
+/*
+
+https://adventofcode.com/
+
+KOTLIN version
+
+ */
+fun main() {
+
+    println("\n--------- 2020 ---------")
+    `2020Day01`().run();
+    `2020Day02`().run();
+    `2020Day03`().run();
+
+    println("\n--------- 2021 ---------")
+    y2021.`2021Day01`().run();
+    y2021.`2021Day02`().run();
+
+
+}
+
+
+
+
