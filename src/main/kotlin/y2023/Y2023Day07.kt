e7cbae07386d0b7230f516bc2cf801574308b16a
Andreas Lönnholm <andreas.l@appcorn.se>
2023-12-14 08:34:34 +0100

    2023-12-14

--- src/main/kotlin/y2023/Y2023Day07.kt
+++ src/main/kotlin/y2023/Y2023Day07.kt
@@ -2,7 +2,8 @@ package y2023
 import utils.Utils
 
 private fun main() {
-    Y2023Day07().part2()
+    Y2023Day07().part1() // SUCCESS > 251058093
+    Y2023Day07().part2() // SUCCESS > 249781879
 }
 
 class Y2023Day07 {
@@ -46,7 +47,7 @@ class Y2023Day07 {
     fun part2() {
 
         val cardsWithTypes = camelCards.map { card ->
-            val cardType = whichHand(card.first())
+            val cardType = whichHand(card.first(), true)
             CardWithType(card, cardType)
         }
 
@@ -65,23 +66,55 @@ class Y2023Day07 {
             }
             .reduce { acc, value -> acc + value }.toInt()
 
-        println("order " + score )
+        println("Score " + score )
 
     }
 
-    private fun whichHand(card: String): CardType {
-        val counts = card.groupingBy { it }.eachCount()
-        val distinctCards = card.toSet().size
+    private fun whichHand(card: String, isPart2: Boolean = false): CardType {
 
-        return when {
-            distinctCards == 1 -> CardType.FiveOfAKind
-            counts.any { it.value == 4 } -> CardType.FourOfAKind
-            counts.containsValue(3) && counts.containsValue(2) -> CardType.FullHouse
-            counts.any { it.value == 3 } -> CardType.ThreeOfAKind
-            counts.values.count { it == 2 } == 2 -> CardType.TwoPair
-            counts.containsValue(2) -> CardType.OnePair
-            distinctCards == 5 -> CardType.HighCard
-            else -> CardType.Error
+        // Get unique cards from the hand
+        val unique = card.toSet()
+
+        // Count occurrences of each card
+        val counts = card.groupingBy { it }.eachCount()
+
+        // Get the count of Jokers in the hand
+        val jokerCount = counts['J']
+
+        return if (isPart2) when (unique.size) {
+            1 -> CardType.FiveOfAKind
+            2 -> if (counts.any { it.value == 4 }) if ('J' in unique) CardType.FiveOfAKind else CardType.FourOfAKind
+            else when (jokerCount) {
+                in 2..3 -> CardType.FiveOfAKind
+                else -> CardType.FullHouse
+            }
+            3 -> if (counts.count { it.value == 2 } == 2) when (jokerCount) {
+                1 -> CardType.FullHouse
+                2 -> CardType.FourOfAKind
+                else -> CardType.TwoPair
+            }
+            else when (jokerCount) {
+                1, 3 -> CardType.FourOfAKind
+                else -> CardType.ThreeOfAKind
+            }
+            4 -> when (jokerCount) {
+                1, 2 -> CardType.ThreeOfAKind
+                else -> CardType.OnePair
+            }
+            else -> if (jokerCount == 1) CardType.OnePair else CardType.HighCard
+        }
+        else when {
+            unique.size == 1 -> CardType.FiveOfAKind
+            card.groupingBy { it }.eachCount().any { it.value == 4 } -> CardType.FourOfAKind
+            card.groupingBy { it }.eachCount().containsValue(3) && card.groupingBy { it }.eachCount().containsValue(2) -> CardType.FullHouse
+            card.groupingBy { it }.eachCount().any { it.value == 3 } -> CardType.ThreeOfAKind
+            card.groupingBy { it }.eachCount().values.count { it == 2 } == 2 -> CardType.TwoPair
+            card.groupingBy { it }.eachCount().containsValue(2) -> CardType.OnePair
+            unique.size == 5 -> CardType.HighCard
+            else -> {
+                println("Error! This should NEVER happen")
+                CardType.Error
+            }
         }
     }
 
@@ -98,7 +131,7 @@ class Y2023Day07 {
 
     private fun cardValue(card: Char, isPart2: Boolean = false): Int {
 
-        return when (card) {
+        val result =  when (card) {
             'A' -> 14
             'K' -> 13
             'Q' -> 12
@@ -107,6 +140,8 @@ class Y2023Day07 {
             in '2'..'9' -> card.toString().toInt()
             else -> -1
         }
+
+        return result
     }
 
 }
